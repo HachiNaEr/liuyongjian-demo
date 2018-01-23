d3669451768e832bfd6bee06aa327a3f783b65eb
liuyongjian <liuyongjian@sqhtech.com>
2018-01-23 21:06:02 +0800

    增加关于类型转换Test

--- src/main/java/liuyongjian/demo/base/ByteConvert.java
+++ src/main/java/liuyongjian/demo/base/ByteConvert.java
@@ -30,12 +30,38 @@ public class ByteConvert {
 	public void ac() {
 		int a = 228; // 00000000 00000000 00000000 11100100
 		byte b = -28;
-		int c = -28;
 		// 1. byte转换为int 11111111 11111111 11111111 11100100
-		// 2. int & 0xFF 11111111 11111111 11111111 11100100 & 00000000 00000000 00000000 11111111
+		// 2. int & 0xFF 去除前24位
+		// 11111111 11111111 11111111 11100100 & 00000000 00000000 00000000 11111111
 		// 3. result: 00000000 00000000 00000000 11100100
 		assertEquals(b & 0xFF, a);
-		assertEquals(b, c);
 	}
 	
+	@Test
+	public void ad() {
+		int a = 130; // int截断24高位, 判断剩余8位首位，若为1则为负数，计算后7位补码，若为0不计算后7位
+		assertEquals((byte)a, -126);
+	}
+	
+	@Test
+	public void ae() {
+		// 任一中文
+		String chineseChar = "昺";
+		byte[] bytes = chineseChar.getBytes();
+		
+		assertEquals("111", Integer.toBinaryString(bytes[0] & 0xFF).substring(0, 3));
+		assertEquals("10",  Integer.toBinaryString(bytes[1] & 0xFF).substring(0, 2));
+		assertEquals("10",  Integer.toBinaryString(bytes[2] & 0xFF).substring(0, 2));
+	}
+	
+	@Test
+	public void af() {
+		long l = 100000000000000l;
+		int i = (int) l;
+		assertEquals(276447232, i);
+		
+		int a = 10;
+		long b = a;
+		assertEquals(10, b);
+	}
 }
---
+++ src/main/java/liuyongjian/demo/base/DataEncryption.java
@@ -0,0 +1,63 @@
+package liuyongjian.demo.base;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataEncryption {
+	private final static String DES = "DES";
+	private final static String SHA256 = "SHA-256"; // MD5、SHA256
+
+	@Test
+	public void aa() {
+		try {
+			MessageDigest messageDigest = MessageDigest.getInstance(SHA256);
+			byte[] bytes = messageDigest.digest("abc".getBytes());
+			StringBuffer sb = new StringBuffer();
+			for (int index = 0; index < bytes.length; index++) {
+				sb.append(Integer.toHexString(bytes[index] & 0xFF));
+			}
+			assertEquals("ba7816bf8f1cfea414140de5dae2223b0361a396177a9cb410ff61f2015ad", sb.toString());
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		// DES算法要求有一个可信任的随机数源
+		SecureRandom sr = new SecureRandom();
+		// 从原始密匙数据创建DESKeySpec对象
+		DESKeySpec dks = new DESKeySpec("12345678".getBytes());
+		// 创建一个密匙工厂，然后用它把DESKeySpec转换成一个SecretKey对象
+		SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
+		SecretKey securekey = keyFactory.generateSecret(dks);
+		// Cipher对象实际完成加密操作
+		Cipher cipher = Cipher.getInstance(DES);
+		// 用密匙初始化Cipher对象
+		cipher.init(Cipher.ENCRYPT_MODE, securekey, sr);
+		// 执行加密操作
+		byte[] b = cipher.doFinal("aaa".getBytes());
+		
+		StringBuffer sb = new StringBuffer();
+		for (int n = 0; n < b.length; n++) {
+			sb.append(java.lang.Integer.toHexString(b[n] & 0XFF));
+		}
+		String result = sb.toString().toUpperCase();
+
+		System.out.println(result);
+	}
+
+}
