5bebd647803d5deb43941e9480c5e96cfec9f710
liuyongjian <liuyongjian@sqhtech.com>
2018-03-29 17:50:46 +0800

    学习链表:)

--- src/main/java/liuyongjian/demo/algorithm/LLinkedList.java
+++ src/main/java/liuyongjian/demo/algorithm/LLinkedList.java
@@ -1,149 +1,102 @@
 package liuyongjian.demo.algorithm;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
-public class LLinkedList<E> implements List<E> {
+public class LLinkedList<E> {
+	private Node<E> first;
+
+	private Node<E> last;
+
+	private int size;
+
+	// 添加第一个节点
+	public void addFirst(E e) {
+		Node<E> f = first;
+		Node<E> newNode = new Node<>(null, e, f);
+		first = newNode;
+		if (f == null) {
+			last = newNode;
+		} else {
+			f.prev =  newNode;
+		}
+		size++;
+	}
+
+	public void addLast(E e) {
+		Node<E> l = last;
+		Node<E> newNode = new Node<>(last, e, null);
+		last = newNode;
+		if (l == null) {
+			first = newNode;
+		} else {
+			l.next = newNode;
+		}
+		size++;
+	}
 	
+	public E getFirst() {
+		Node<E> f = first;
+		if (f == null) {
+			throw new NoSuchElementException();
+		}
+		return first.item;
+	}
+
+	public E getLast() {
+		Node<E> l = last;
+		if (l == null) {
+			throw new NoSuchElementException();
+		}
+		return last.item;
+	}
+
+	public void removeFirst() {
+		if (first == null) {
+			throw new NoSuchElementException();
+		}
+		Node<E> f = first.next;
+		f.prev = null;
+		first = f;
+		size--;
+	}
 	
-	@Override
+	public void removeLast() {
+		if (last == null) {
+			throw new NoSuchElementException();
+		}
+		Node<E> l = last.prev;
+		l.next = null;
+		last = l;
+		size--;
+	}
+	
+	@SuppressWarnings("hiding")
+	class Node<E> {
+		private E item;
+		private Node<E> prev;
+		private Node<E> next;
+
+		public Node(Node<E> prev, E item, Node<E> next) {
+			this.prev = prev;
+			this.item = item;
+			this.next = next;
+		}
+	}
+
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean contains(Object o) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T> T[] toArray(T[] a) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean add(E e) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean remove(Object o) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean containsAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean addAll(Collection<? extends E> c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean addAll(int index, Collection<? extends E> c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean removeAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean retainAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public E get(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public E set(int index, E element) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void add(int index, E element) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public E remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int indexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int lastIndexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public ListIterator<E> listIterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ListIterator<E> listIterator(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<E> subList(int fromIndex, int toIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		for (Node<E> next = first; next != null;) {
+			Node<E> node = next.next;
+			next.prev = null;
+			next.item = null;
+			next.next = null;
+			next = node;
+		}
+		first = last = null;
+		size = 0;
 	}
 
 }
